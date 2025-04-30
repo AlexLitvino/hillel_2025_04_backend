0a0a958ce2707ea7870e17dd0fefce6a77a3332f
AlexLitvino <litvinov.aleks@gmail.com>
2025-04-30 15:31:07 +0300

    added logic to view all, view specific student and add student

--- digital_journal/journal.py
+++ digital_journal/journal.py
@@ -7,7 +7,7 @@
 import json
 
 with open('students.json', 'r') as f:
-    storage = json.load(f)
+    storage: list[dict] = json.load(f)
 
 
 
@@ -19,31 +19,50 @@ with open('students.json', 'r') as f:
   - `add_student(name: str, marks: list[int], details: str | None)`
 """
 
+def get_max_id():
+    return max([student['id'] for student in storage])
 
 def show_students():
     for student in storage:
         print('===================\n'
-              f'{student["name"]}\n'
+              f'{student["id"]}. {student["name"]}\n'
               f'Marks: {" ".join([str(i) for i in student["marks"]])}')
+    print()
 
 
-def show_student(id: int):
-    pass
+def show_student(id_: int):
+    student = [student for student in storage if student['id'] == id_][0]
+    print('===================\n'
+          f'{student["name"]}\n'
+          f'Marks: {" ".join([str(i) for i in student["marks"]])}\n'
+          f'Information: {student["info"]}\n')
+
+def parse_add_student_input(add_student_input:str):
+    if add_student_input.count(';') == 0:
+        name = add_student_input
+        details = ''
+    else:
+        name, details = add_student_input.split(';')
+    return name, details
 
 
 def add_student(name: str, marks: list[int], details: str | None):
-    pass
+    student = {"id": get_max_id() + 1,
+               "name": name,
+               "marks": [],
+               "info": details if details else ""}
+    storage.append(student)
 
 
 STUDENT_MANAGEMENT_COMMANDS = ('add', 'show all', 'show')
 AUXILIARY_COMMANDS = ('help', 'quit')
-
+COMMAND_LIST = ', '.join((*STUDENT_MANAGEMENT_COMMANDS, *AUXILIARY_COMMANDS))
 
 def main():
     print("Welcome to DIGITAL JOURNAL APP\n")
 
     while True:
-        command = input(f"Enter one of the commands: {(*STUDENT_MANAGEMENT_COMMANDS, *AUXILIARY_COMMANDS)}: ")
+        command = input(f"Enter one of the commands: {COMMAND_LIST}: ").strip().lower()
         match command:
             case 'quit':
                 break
@@ -51,20 +70,29 @@ def main():
                 print('This is Digital Journal App\n'
                       'It helps to manage students assessment')
             case 'add':
-                ask_prompt = "Enter student's payload data using text template\n" \
-                             "John Doe;1,2,3,4,5\n"
-                add_student()
-            case 'show': # TODO: id is missing in storage
+                ask_prompt = "Enter student's payload data using text template (name is obligatory field, details is optional field)\n" \
+                             "John Doe;Some details about John:\n"
+                add_student_input = input(ask_prompt).strip()
+                if add_student_input.count(';') > 1:
+                    print("Student's payload data could contain one or zero semicolon")
+                else:
+                    name, details = parse_add_student_input(add_student_input)
+                    add_student(name, marks=None, details=details)  # TODO: no need to pass marks?
+                print()
+            case 'show':
                 id_string = input("Enter student's id to display info: ")
                 try:
-                    id = int(id_string)
-                    show_student(id)
+                    id_ = int(id_string)
+                    if id_ in [student["id"] for student in storage]:
+                        show_student(id_)
+                    else:
+                        print(f"Student with id={id_} is missing in the students list\n")
                 except ValueError:
-                    print("Entered value is not student id")
+                    print("Entered value is not valid student id. It should be integer\n")
             case 'show all':
                 show_students()
             case _:
-                print("Unknown command")
+                print("Unknown command\n")
 
 
 if __name__ == '__main__':
