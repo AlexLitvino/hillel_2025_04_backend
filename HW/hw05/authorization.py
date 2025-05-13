b8b804c767bfa4e053a5e9a62a52f8152f15fe73
AlexLitvino <litvinov.aleks@gmail.com>
2025-05-13 11:05:52 +0300

    added solution for authorization

--- HW/hw05/authorization.py
+++ HW/hw05/authorization.py
@@ -11,37 +11,40 @@ users = [
     User('Bob', 'qwerty')
 ]
 
-"""
-- if the function is decorated with `auth` - the application requires `username` and `password` to be entered by user (think about UX by yourself)
-  - ASK for credentials UNTIL they are NOT CORRECT
-  - IF credentials are CORRECT - EXECUTE the command (actually just prints the command)
-  - IF User has entered credentials CORRECTLY once - it is CACHED and used to call future commands WITHOUT additional AUTHORIZATION
-  
-  
-  while user_input := input("Enter anything: "):
-    command(user_input)
-"""
 
 def auth(func):
+    _authorized = []
 
     def wrapper(*args, **kwargs):
         username = input('Enter username: ').strip()
-        password = input('Enter password: ').strip()
-        expected_password = [user.password for user in users if user.username == username][0]
-        if username NOT IN users:
+        if username in _authorized:
+            print(f'Authorized from cache as {username}')
+            result = func(*args, **kwargs)
+            return result
+        elif username not in [user.username for user in users]:
+            print('User is not registered in users')
             return
-        if entered_password != expected_password:
-            continue (3 times?)
         else:
-            cache_password
-        result = func(*args, **kwargs)
-        return result
+            expected_user = [user for user in users if user.username == username][0]
+
+            while entered_password := input('Enter password: ').strip():
+                if entered_password == expected_user.password:
+                    _authorized.append(expected_user.username)
+                    print(f'Authorized as {expected_user.username}')
+                    result = func(*args, **kwargs)
+                    return result
+                else:
+                    print('Password is wrong!')
+
     return wrapper
 
 
+if __name__ == '__main__':
 
-@auth
-def command(payload):
-    print(f"Executing command by authorized user.\nPayload: {payload}")
+    @auth
+    def command(payload):
+        print(f"Executing command by authorized user.\nPayload: {payload}")
 
-command('111-111-111')
+    command('111-111-111')
+    command('222-222-222')
+    command('333-333-333')
