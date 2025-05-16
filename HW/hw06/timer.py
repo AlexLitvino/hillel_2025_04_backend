3d1eca04e6e64d5cd4610e98b4d0267b748d0fae
AlexLitvino <litvinov.aleks@gmail.com>
2025-05-16 12:46:42 +0300

    upated timer from usage time to perf_counter

--- HW/hw06/timer.py
+++ HW/hw06/timer.py
@@ -3,17 +3,19 @@ import logging
 
 class TimerContext:
     def __enter__(self):
-        self.start = time.time()
+        self.start = time.perf_counter()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        elapsed = time.time() - self.start
+        elapsed = time.perf_counter() - self.start
         logging.info(f"Elapsed: {elapsed:.2f} seconds")
 
 logging.basicConfig(level=logging.INFO)
 
-with TimerContext():
-    time.sleep(2)
 
-with TimerContext():
-    time.sleep(4)
+if __name__ == '__main__':
+    with TimerContext():
+        time.sleep(2)
+
+        with TimerContext():
+            time.sleep(4)
