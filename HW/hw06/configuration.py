fb46e1454694afe45657b38596338d24c6d1978d
AlexLitvino <litvinov.aleks@gmail.com>
2025-05-16 13:56:47 +0300

    added solution for configuration context manager

--- HW/hw06/configuration.py
+++ HW/hw06/configuration.py
@@ -2,20 +2,46 @@ GLOBAL_CONFIG = {"feature_a": True, "max_retries": 3}
 
 class Configuration:
     def __init__(self, updates, validator=None):
-        pass
+        self.updates = updates
+        self.validator = validator
 
     def __enter__(self):
-        pass
+        self.global_config = GLOBAL_CONFIG.copy()
+        if self.validator:
+            if not self.validator(self.updates):
+                raise Exception('Config for update is not valid')
+        GLOBAL_CONFIG.update(self.updates)
 
     def __exit__(self, exc_type, exc_value, traceback):
-        pass
+        global GLOBAL_CONFIG
+        GLOBAL_CONFIG = self.global_config
 
 def validate_config(config):
     # Ensure max_retries >= 0
     return config.get("max_retries", 0) >= 0
 
-"""
-- Accepts updates as a dictionary.
-- Restores original configuration even if an error occurs.
-- Optionally accepts a validator function.
-"""
+
+if __name__ == '__main__':
+
+    # No validation
+    with Configuration({"feature_a": False, "feature_b": True}):
+        print(GLOBAL_CONFIG)
+
+    print(GLOBAL_CONFIG)
+    print()
+
+    # Successful validation
+    with Configuration({"feature_a": False, "feature_c": True, "max_retries": 4}, validator=validate_config):
+        print(GLOBAL_CONFIG)
+
+    print(GLOBAL_CONFIG)
+    print()
+
+    # Unsuccessful validation
+    try:
+        with Configuration({"max_retries": -1}, validator=validate_config):
+            print(GLOBAL_CONFIG)
+    except Exception as e:
+        print(e)
+
+    print(GLOBAL_CONFIG)
