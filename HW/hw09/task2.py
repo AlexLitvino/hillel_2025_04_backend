ba9975a7d30f0bda9c0d29dfc10bfee48fa59144
AlexLitvino <litvinov.aleks@gmail.com>
2025-06-10 00:07:28 +0300

    partial implementation

--- HW/hw09/task2.py
+++ HW/hw09/task2.py
@@ -12,6 +12,8 @@ you're building a simple backend moderation system for post comments
 - the system should support exporting flagged comments to a local JSON file called `flagged_comments.json`
 - handle HTTP errors gracefully and skip any malformed data entries
 """
+import re
+
 from api_client import Client
 
 BASE_URL = "https://jsonplaceholder.typicode.com"
@@ -23,13 +25,19 @@ class Comment:
         self.name = name
         self.email = email
         self.body = body
+        self.is_suspicious = None
+
 
 class CommentModerator:
+
+    SUSPICIOUS_CONTENT = ["buy", "free", "offer", "!!", "omnis", "autem", "quis"]
+
     def __init__(self, client: Client):
         self._client: Client = client
         self.comments: list[Comment] = []
         self.flagged_comments: list[Comment] = []
         self.fetch_comments()
+        self.flag_suspicious_comments()
 
     def fetch_comments(self):
         self.comments = [Comment(comment['id'],
@@ -40,19 +48,55 @@ class CommentModerator:
                          for comment in self._client.get_all_comments()]
 
     def flag_suspicious_comments(self):
-        pass
+        for comment in self.comments:
+            if self._is_suspicious(comment.body):
+                setattr(comment, 'is_suspicious', True)
+            else:
+                setattr(comment, 'is_suspicious', False)
+
 
     def group_by_post(self) -> dict[int, list[Comment]]:
-        pass
+        from collections import defaultdict
+        suspicious_comments_grouped_by_post = defaultdict(list)
+        for comment in self.comments:
+            if comment.is_suspicious:
+                suspicious_comments_grouped_by_post[comment.post_id].append(comment)
+        return suspicious_comments_grouped_by_post
 
     def top_spammy_emails(self, n: int = 5) -> list[str]:
-        pass
+        from collections import Counter
+        spammers = Counter([comment.email for comment in self.comments if comment.is_suspicious])
+        print(spammers)
+        print(spammers.total())
+        return spammers.most_common(n)
+
+
 
     def export_flagged_to_json(self, filename: str = "flagged_comments.json"):
         pass
 
 
+    @staticmethod
+    def _is_suspicious(comment):
+        words_re = re.compile("|".join(CommentModerator.SUSPICIOUS_CONTENT))
+        return words_re.search(comment)
+
+
+    def print_comments_debug(self):
+        for comment in self.comments:
+            print(f'{comment.is_suspicious}:{comment.post_id} {comment.body}')
+            print()
+
+
+
 if __name__ == '__main__':
-    client = Client(BASE_URL)
-    comments = client.get_all_comments()
-    print(comments)
+    from pprint import pprint
+    api_client = Client(BASE_URL)
+    comment_moderator = CommentModerator(api_client)
+    comment_moderator.print_comments_debug()
+
+    pprint(comment_moderator.group_by_post())
+
+    print(comment_moderator.top_spammy_emails())
+
+
